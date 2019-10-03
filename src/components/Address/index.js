d2286878a24a71ff4f07552dc59b23946311d565
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-03 08:47:04 -0300

    :earth_americas: Informações de endereço

--- src/components/Address/index.js
+++ src/components/Address/index.js
@@ -4,9 +4,8 @@ import * as S from './styled';
 const Address = ({ bairro, cep, localidade, uf, logradouro }) => {
 	return(
 		<S.Adress>
-			<S.AdressTitle>
-				{logradouro}
-			</S.AdressTitle>
+			<S.AdressTitle>{logradouro}</S.AdressTitle>
+
 			<p>{bairro}</p>
 			<p>{localidade} - {uf}</p>
 			<p>{cep}</p>
