36975d64d7c2e4112585463a90c7b09be389b791
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-03 08:49:36 -0300

    :wrench: Melhoria e criação de funções para a manipulação de informações entre os componentes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from './components/Card';
 import GlobalStyles from './components/styles/global';
@@ -7,19 +7,27 @@ import Map from './components/Map';
 import Address from './components/Address';
 
 function App() {
+	const [closeMap, setCloseMap] = useState(true);
+	const [addressInfo, setAddressInfo] = useState(null);
+
+	const [formError, setformError] = useState(null);
 
 	return (
 		<div className="App">
 			<GlobalStyles />
 
 			<Card title="Consultar seu CEP">
-				<Form />
+				<Form
+					formError={formError}
+					setformError={setformError}
+					setAddressInfo={setAddressInfo}
+				/>
 			</Card>
 
-			<Card>
-				<Address />
-				<Map />
-			</Card>
+				<Card>
+					{addressInfo && (<Address {...addressInfo} />)}
+					{!closeMap && (<Map setCloseMap={setCloseMap} />)}
+				</Card>
 		</div>
 	);
 }
