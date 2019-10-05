406e68457ad71f89dc1b7892e33db0d9dacbedde
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-05 12:56:16 -0300

    :pushpin: Component Marker

---
+++ src/components/MapMarker/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styled';
+
+const MapMarker = ({ text }) => (
+	<S.mapMarker>
+		{text}
+	</S.mapMarker>
+);
+
+export default MapMarker;
---
+++ src/components/MapMarker/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const mapMarker = styled.div`
+	align-items: center;
+	background: #d0d;
+	border-radius: 300px;
+	color: #fff;
+	display: inline-flex;
+	justify-content: center;
+	padding: 15px 10px;
+	text-align: center;
+	transform: translate(-50%, -50%);
+`;
